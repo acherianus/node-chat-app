0bada64f134f11c9d7e6ff1dfbc4ac929e05082d
acherianus <acherianus@gmail.com>
2018-03-21 22:01:32 -0400

    add geo location

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require("./message");
+var {generateMessage, generateLocationMessage} = require("./message");
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -10,5 +10,19 @@ describe('generateMessage', () => {
 
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location message object', () => {
+    var from = 'Apc';
+    var longitude = 2;
+    var latitude = 1;
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    var expectedUrl = `https://www.google.com/maps/?q=${latitude},${longitude}`;
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url:expectedUrl});
   });
 });
